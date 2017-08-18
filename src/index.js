fc8cbf03f18109aca1b357d70df9fd1718f6f31f
hunkid <273946286@qq.com>
2017-08-18 10:46:49 +0800

    上一次Git出错，以此commit为准

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Route, IndexRoute, hashHistory, Router } from 'react-router'
 // import { Provider } from 'react-redux'
-import App from './containers/App'
-import AllMemosRoute from './routes/AllMemosRoute'
-import TodoRoute from './routes/TodoRoute'
-import DoingRoute from './routes/DoingRoute'
-import DoneRoute from './routes/DoneRoute'
+import App from './containers/App/App'
+import ListAllMemos from './containers/ListAllMemos/ListAllMemos'
+import ListTodoMemos from './containers/ListTodoMemos/ListTodoMemos'
+import ListDoingMemos from './containers/ListDoingMemos/ListDoingMemos'
+import ListDoneMemos from './containers/ListDoneMemos/ListDoneMemos'
 // import configureStore from './stores'
 // import './main.less'
 
@@ -15,10 +15,10 @@ ReactDOM.render(
   // <Provider store={store}>
   <Router history={hashHistory}>
     <Route path="/" component={App}>
-      <IndexRoute component={AllMemosRoute} /> 
-      <Route path="/todo" component={TodoRoute} />
-      <Route path="/doing" component={DoingRoute} />
-      <Route path="/done" component={DoneRoute} />
+      <IndexRoute component={ListAllMemos} /> 
+      <Route path="/todo" component={ListTodoMemos} />
+      <Route path="/doing" component={ListDoingMemos} />
+      <Route path="/done" component={ListDoneMemos} />
     </Route>
   </Router>,
     // </Provider>,
